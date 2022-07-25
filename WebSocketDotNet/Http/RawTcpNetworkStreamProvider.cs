f213d462f89991ee1dd8718d272f2baf3fecaab2
Sam Byass <sam@samboycoding.me>
2022-07-25 15:54:53 +0100

    [publish] 0.5.5 - Hard reset tcp client on every call to GetStream

--- WebSocketDotNet/Http/RawTcpNetworkStreamProvider.cs
+++ WebSocketDotNet/Http/RawTcpNetworkStreamProvider.cs
@@ -9,7 +9,7 @@ namespace WebSocketDotNet.Http;
 
 internal class RawTcpNetworkStreamProvider : NetworkStreamProvider
 {
-    private TcpClient _client;
+    private TcpClient? _client;
     private NetworkStream? _lastStream;
     
     public override bool AnythingToRead => _lastStream?.DataAvailable ?? false;
@@ -17,18 +17,21 @@ internal class RawTcpNetworkStreamProvider : NetworkStreamProvider
 
     public RawTcpNetworkStreamProvider(string host, int port) : base(host, port)
     {
+    }
+
+    private void ResetClient()
+    {
+        _client?.Close();
+        
         _client = new();
+        _lastStream = null;
     }
 
     public override Stream GetStream()
     {
-        if (_client.Connected)
-        {
-            _client.Close();
-            _client = new();
-        }
+        ResetClient();
 
-        _client.Connect(Host, Port);
+        _client!.Connect(Host, Port);
 
         return _lastStream = _client.GetStream();
     }
@@ -36,7 +39,9 @@ internal class RawTcpNetworkStreamProvider : NetworkStreamProvider
 #if SUPPORTS_ASYNC
     public override async Task<Stream> GetStreamAsync()
     {
-        await _client.ConnectAsync(Host, Port);
+        ResetClient();
+        
+        await _client!.ConnectAsync(Host, Port);
         
         return _lastStream = _client.GetStream();
     }
